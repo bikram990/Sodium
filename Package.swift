5812a3d879b77aae0fdfbd62d0e8354e914d15ae
Erik Little <nuclear.ace@gmail.com>
2017-09-20 07:58:31 -0400

    swift 4

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Sodium",
-    pkgConfig: "libsodium"
+    pkgConfig: "libsodium",
+        products: [
+        .library(name: "Sodium", targets: ["Sodium"])
+    ],
+    targets: [
+        .target(name: "Sodium", publicHeadersPath: "./shim.h")
+    ]
 )
